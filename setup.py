7467f21182e1b5d940ae3f106a1405da693b2a42
Victor Torres <vpaivatorres@gmail.com>
2018-03-11 20:45:02 -0300

    releasing 0.0.6

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='python-redis-rate-limit',
-    version='0.0.5',
+    version='0.0.6',
     description=u'Python Rate Limiter based on Redis.',
     long_description=readme,
     author=u'Victor Torres',
